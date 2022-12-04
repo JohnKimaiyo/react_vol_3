1841d20904455e11ee8d2ef634736904d6b1bfe1
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-04 21:49:29 +0300

    greet and welcome compenents rendered

---
+++ src/Component/Welcome.js
@@ -0,0 +1,9 @@
+import React ,{Component}from 'react';
+
+class welcome extends Component{
+    render(){
+        return <h1>Welcome Node Developer Kimaiyo</h1>
+    }
+}
+
+export default welcome;
