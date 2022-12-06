ece339f2d8e3ddf2e98fbdc99d2cd404da3dbb0c
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-06 09:21:54 +0300

    jsx called

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import Welcome from './Component/Welcome';
 import Greet from './Component/Greet';
-
+import Hello from './Component/Hello';
 function App() {
   return (
     <div className="App">
       <Greet/>
       <Welcome/>
+      <Hello/>
     </div>
   );
 }
