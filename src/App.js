aa773e1f05475c369006423eb91126bf65e683c6
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-06 18:27:41 +0300

    props tile called

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import './App.css';
-import Welcome from './Component/Welcome';
-import Greet from './Component/Greet';
-import Hello from './Component/Hello';
+import React from "react";
+import "./App.css";
+import Welcome from "./Component/Welcome";
+import Greet from "./Component/Greet";
+import Hello from "./Component/Hello";
+import Title from "./Component/Title";
 function App() {
   return (
     <div className="App">
-      <Greet/>
-      <Welcome/>
-      <Hello/>
+      <Greet />
+      <Welcome />
+      <Hello />
+      <Title name="FrontEnd Developer Kimaiyo" />
+      <Title name="Mobile  Developer Kimaiyo" />
+      <Title name="Solidity Developer Kimaiyo" />
     </div>
   );
 }
