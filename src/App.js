45684c0be1720778dc4df7ecdc4efbecd909bafc
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-14 22:08:12 -0800

    booklist component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import Booklist from "./Component/BookList";
 import Navbar from "./Component/Navbar";
 
 function App() {
   return <div className="App">
   <Navbar/>
-  
+  <Booklist/>
   </div>;
 }
 
---
+++ src/Component/BookList.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+
+class Booklist extends Component{
+    render(){
+        return(
+            <di className="book=list">
+            <ul>
+            <li>the way of kings</li>
+            <li>the anem of th wind</li>
+            <li>the final empire</li>
+            </ul>
+            </di>
+        )
+    }
+}
+
+export default Booklist;
