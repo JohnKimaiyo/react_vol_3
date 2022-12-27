1185aedb469ff0c69ada731ad0b9e0954aff98ec
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-27 12:49:32 +0300

    usestate

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from "react";
 import "./App.css";
-import Booklist from "./Component/BookList";
-import Navbar from "./Component/Navbar";
-import ThemeToggle from "./Component/ThemeToggle";
-import ThemeContextProvider from "./context/ThemeContext";
+
+import ClickMe from "./useState/ClickMe";
 
 function App() {
   return (
     <div className="App">
-      <ThemeContextProvider>
-        <Navbar />
-        <Booklist />
-        <ThemeToggle/>
-      </ThemeContextProvider>
+      <ClickMe />
     </div>
   );
 }
---
+++ src/useState/ClickMe.js
@@ -0,0 +1,12 @@
+import React ,{useState}from "react";
+
+export default function ClickMe() {
+  // declare a new state variable which we call "count"
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click Me</button>
+    </div>
+  );
+}
